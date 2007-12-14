a08401c04f8193d3fb326304b6d1fa47fc3250da
Ian Fore <forei@mail.nih.gov>
2007-12-14 03:25:03 +0000

    caTissue Text Loader
    SVN-Revision: 3

---
+++ branches/IMF/caTissueCoreLoader/src/loader/ReadColumnMap.java
@@ -0,0 +1,171 @@
+/* THIS caBIGª SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED 
+ * WARRANTIES (INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE) 
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE NATIONAL CANCER INSTITUTE
+ * OR ITS AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS caBIGª
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package loader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import net.sf.flatpack.util.ParserUtils;
+
+import loader.Column;
+import loader.ColumnMap;
+import loader.ValueMap;
+
+
+public class ReadColumnMap {
+
+	private List data;
+	//private String[] data;
+	private ColumnMap cMap;
+	private HashMap<String,Column> colMap = new HashMap<String,Column>();
+	private DateFormat df;
+	
+
+	public void getColumnMapFromFile(String path) {
+		
+        // create a JAXBContext capable of handling classes generated into
+        // the loader package
+        JAXBContext jc;
+		try {
+			jc = JAXBContext.newInstance( "loader",
+					Thread.currentThread().getContextClassLoader() );
+			// create an Unmarshaller
+	        Unmarshaller u = jc.createUnmarshaller();
+	        
+	        // unmarshal a instance document 
+	        cMap = 
+	            (ColumnMap)u.unmarshal( new FileInputStream( path ) );	        
+	        List colList = cMap.getColumn();
+
+            
+	        System.out.println("\n------Mapping details-----\n");
+	        // iterate over List
+	        for( Iterator iter = colList.iterator(); iter.hasNext(); ) {
+	            Column c = (Column) iter.next();
+	            // Store in hash using an uppercase key so we can do
+	            // a case insensitive search
+	            colMap.put(c.getAttributeName().getValue().toUpperCase(), c);
+	            //colMap.put(c.getAttributeName().toString(), c);
+	            System.out.print(c.getAttributeName());
+	            if (c.getColumnNo() != null)
+	            	System.out.println(" In column: " + c.getColumnNo().intValue());
+	            if (c.getFixedValue() != null)
+	            	System.out.println(" Fixed Value:" + c.getFixedValue());
+	        }
+	        System.out.println("\n--------------------------\n");
+
+			df = new SimpleDateFormat(cMap.getDateFormat());
+
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+ 	}
+	
+	public ColumnMap getColumnMap() {
+		return cMap;
+	}
+	
+	public void setDataArray(List f) {
+		data = f;
+	}
+/*	public void setDataArray(String f[]) {
+		data = f;
+	}
+*/	
+	public String getValue(String modelAttribute) {
+		
+		Column c = colMap.get(modelAttribute.toUpperCase());
+		if (c == null)
+			return null;
+		if (c.getColumnNo() != null) {
+			String fromValue = (String) data.get(c.getColumnNo().intValue()-1);
+			if (c.getValueMapping() != null) {
+				List valueMap = c.getValueMapping().getValueMap();
+				for (Iterator values = valueMap.iterator(); values.hasNext();) {
+					ValueMap v = (ValueMap)values.next();
+					if ( (v.isIgnoreCase() && fromValue.equalsIgnoreCase(v.getFromValue()) )  
+							|| fromValue.equals(v.getFromValue())
+						) {
+						System.out.println(modelAttribute + ":" + fromValue + ":" + v.getToValue());
+						return v.getToValue();
+					}
+				}
+			}
+			//System.out.println(modelAttribute + ":" + fromValue);
+			// if we can't find what was in the value map use what was in the file
+			return fromValue;
+		}
+		else if (c.getFixedValue() != null) {
+			System.out.println(modelAttribute + ":fixed:" + c.getFixedValue());
+			return c.getFixedValue();
+		}
+		// we would drop through to here if 
+		//  no fixed value or column was found
+		return null;
+	}
+	
+	public Date getDate(String modelAttribute) 
+		throws caTissueLoadException {
+		Date d = null;
+		String value = getValue(modelAttribute);
+		if (value == null)
+			return null;
+		if (getValue(modelAttribute).equals(""))
+			return d;
+		try {
+			d =  df.parse(getValue(modelAttribute));
+		} catch (ParseException e) {
+			caTissueLoadException le = new caTissueLoadException(e, modelAttribute);
+			throw(le);
+		}
+		return d;
+	}
+	
+	public boolean getReadHeader() {
+		return cMap.isHeaderRow();
+	}
+	
+	public List<String> getRowClasses() {
+		return (List<String>)cMap.getRowClass();
+	}
+	
+	public char getColDelimiter() {
+		if (cMap.getFileType().equalsIgnoreCase("xlText"))
+			return '\t';
+		if (cMap.getFileType().equalsIgnoreCase("excelCSV"))
+			return ',';
+		
+		//default - better than nothing!
+		return '\t';
+	}
+
+	public char getTextQualifier() {
+		return '"' ;
+	}
+}
---
+++ branches/IMF/caTissueCoreLoader/src/net/sf/flatpack/util/ParserUtils.java
@@ -0,0 +1,171 @@
+package net.sf.flatpack.util;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *  Static utilities that are used to perform parsing in the
+ *         DataSet class These can also be used for low level parsing, if not
+ *         wishing to use the DataSet class.
+ *
+ * @author Paul Zepernick
+ * @author Benoit Xhenseval
+ * 
+ * @author Ian Fore
+ * Severely cutdown version of ParserUtils class from FlatPack
+ * see flatpack.sourceforge.net for the full package
+ */
+public final class ParserUtils {
+    private ParserUtils() {
+    }
+
+    /**
+     * Returns an ArrayList of items in a delimited string. If there is no
+     * qualifier around the text, the qualifier parameter can be left null, or
+     * empty. There should not be any line breaks in the string. Each line of
+     * the file should be passed in individually.
+     * Elements which are not qualified will have leading and trailing white
+     * space removed.  This includes unqualified elements, which may be
+     * contained in an unqualified parse: "data",  data  ,"data"
+     *
+     * Special thanks to Benoit for contributing this much improved speedy parser :0)
+     *
+     * @author Benoit Xhenseval
+     * @param line -
+     *            String of data to be parsed
+     * @param delimiter -
+     *            Delimiter seperating each element
+     * @param qualifier -
+     *            qualifier which is surrounding the text
+     * @param initialSize -
+     *            intial capacity of the List size
+     * @return List
+     */
+    public static List splitLine(final String line, final char delimiter, final char qualifier, final int initialSize) {
+        final List list = new ArrayList(initialSize);
+
+        if (delimiter == 0) {
+            list.add(line);
+            return list;
+        } else if (line == null) {
+            return list;
+        }
+
+        String trimmedLine;
+        if (delimiter == '\t' || delimiter == ' ') {
+            //skip the trim for these delimiters, doing the trim will mess up the parse
+            //on empty records which contain just the delimiter
+            trimmedLine = line;
+        } else {
+            trimmedLine = line.trim();
+        }
+
+        final int size = trimmedLine.length();
+
+        if (size == 0) {
+            list.add("");
+            return list;
+        }
+
+        boolean insideQualifier = false;
+        char previousChar = 0;
+        int startBlock = 0;
+        int endBlock = 0;
+        boolean blockWasInQualifier = false;
+
+        final String doubleQualifier = String.valueOf(qualifier) + String.valueOf(qualifier);
+        for (int i = 0; i < size; i++) {
+
+            final char currentChar = trimmedLine.charAt(i);
+            if (currentChar != delimiter && currentChar != qualifier) {
+                previousChar = currentChar;
+                endBlock = i + 1;
+                continue;
+            }
+
+            if (currentChar == delimiter) {
+                // we've found the delimiter (eg ,)
+                if (!insideQualifier) {
+                    String trimmed = trimmedLine.substring(startBlock, endBlock > startBlock ? endBlock : startBlock + 1);
+                    if (!blockWasInQualifier) {
+                        trimmed = trimmed.trim();
+                    } else {
+                        //need to run the qualifier replace when it was in qualifier
+                        trimmed = trimmed.replaceAll(doubleQualifier, String.valueOf(qualifier));
+                    }
+
+                    if (trimmed.length() == 1 && (trimmed.charAt(0) == delimiter || trimmed.charAt(0) == qualifier)) {
+                        list.add("");
+                    } else {
+                        list.add(trimmed);
+                    }
+                    blockWasInQualifier = false;
+                    startBlock = i + 1;
+                }
+            } else if (currentChar == qualifier) {
+                if (!insideQualifier && previousChar != qualifier) {
+                    if (previousChar == delimiter || previousChar == 0 || previousChar == ' ') {
+                        insideQualifier = true;
+                        startBlock = i + 1;
+                    } else {
+                        endBlock = i + 1;
+                    }
+                } else {
+                    if (i + 1 < size && delimiter != ' ') {
+                        //this is used to allow unescaped qualifiers to be contained within the element
+                        //do not run this check is a space is being used as a delimiter
+                        //we don't want to trim the delimiter off
+                        //loop until we find a char that is not a space, or we reach the end of the line.
+                        int start = i + 1;
+                        char charToCheck = trimmedLine.charAt(start);
+                        while (charToCheck == ' ') {
+                            start++;
+                            if (start == size) {
+                                break;
+                            }
+                            charToCheck = trimmedLine.charAt(start);
+                        }
+
+                        if (charToCheck != delimiter) {
+                            previousChar = currentChar;
+                            endBlock = i + 1;
+                            continue;
+                        }
+
+                    }
+                    insideQualifier = false;
+                    blockWasInQualifier = true;
+                    endBlock = i;
+                    // last column (e.g. finishes with ")
+                    if (i == size - 1) {
+                        String str = trimmedLine.substring(startBlock, size - 1);
+                        str = str.replaceAll(doubleQualifier, String.valueOf(qualifier));
+                        list.add(str);
+                        startBlock = i + 1;
+                    }
+                }
+            }
+            previousChar = currentChar;
+        }
+
+        if (startBlock < size) {
+            String str = trimmedLine.substring(startBlock, size);
+            str = str.replaceAll(doubleQualifier, String.valueOf(qualifier));
+            if (blockWasInQualifier) {
+                if (str.charAt(str.length() - 1) == qualifier) {
+                    list.add(str.substring(0, str.length() - 1));
+                } else {
+                    list.add(str);
+                }
+            } else {
+                list.add(str.trim());
+            }
+        } else if (trimmedLine.charAt(size - 1) == delimiter) {
+            list.add("");
+        }
+
+        return list;
+    }
+
+ }
