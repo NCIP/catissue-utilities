bc280926ef657abe9f406d0b19bfbcc64e424efd
Ian Fore <forei@mail.nih.gov>
2010-11-23 20:48:25 +0000

    Updated for caTissue Suite 1.1.2
    SVN-Revision: 18

---
+++ trunk/caTissueLoader/src/caTissueUtils/caTissueSession.java
@@ -0,0 +1,39 @@
+package caTissueUtils;
+
+import java.util.Map;
+
+import gov.nih.nci.system.applicationservice.ApplicationService;
+import gov.nih.nci.system.applicationservice.ApplicationServiceProvider;
+import gov.nih.nci.system.comm.client.ClientSession;
+import keychain.CaTissueInstance;
+import keychain.KeyChainLoader;
+
+public class caTissueSession {
+	
+	public static ApplicationService logon(String instanceName, String keyChainName) {
+
+
+		Map<String,CaTissueInstance> instances = KeyChainLoader.loadKeyChain(keyChainName);
+
+		CaTissueInstance instance = instances.get(instanceName);
+
+		String serviceURL = instance.getHost() + "/catissuecore/http/remoteService";
+		System.out.println("Accessing: " + instance.getName());
+		System.out.println("at: " + serviceURL);
+
+		ApplicationService appService = ApplicationServiceProvider.getRemoteInstance(serviceURL);
+
+		ClientSession cs = ClientSession.getInstance();
+		try
+		{ 
+			cs.startSession(instance.getUsername(), instance.getPassword());
+		} 
+		catch (Exception ex) 
+		{ 
+			System.out.println(ex.getMessage()); 
+			ex.printStackTrace();
+		}
+		return appService;
+	}
+
+}
