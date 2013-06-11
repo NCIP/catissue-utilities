3af2addb672946e8a4dba1d773d7a6bcfcd0c998
Prasad Konka <prasad.konka@nih.gov>
2013-06-11 09:37:35 -0400

    Added license header

--- caTissueCoreLoader/src/loader/caTissueLoadException.java
+++ caTissueCoreLoader/src/loader/caTissueLoadException.java
@@ -1,3 +1,13 @@
+/*L
+ *  Copyright Washington University in St. Louis
+ *  Copyright SemanticBits
+ *  Copyright Persistent Systems
+ *  Copyright Krishagni
+ *
+ *  Distributed under the OSI-approved BSD 3-Clause License.
+ *  See http://ncip.github.com/catissue-utilities/LICENSE.txt for details.
+ */
+
 /* THIS caBIGª SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED 
  * WARRANTIES (INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
  * MERCHANTABILITY, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE) 
